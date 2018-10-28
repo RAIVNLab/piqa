4d493ac65d396a64617c2e839bf36c270348c351
Minjoon Seo <seominjoon@gmail.com>
2018-10-28 13:17:02 +0900

    gh-page setup (clean up)

--- squad/base/__init__.py
+++
@@ -1,4 +0,0 @@
-from base.argument_parser import ArgumentParser
-from base.file_interface import FileInterface
-from base.processor import Processor, Sampler
-from base.model import Model, Loss
--- squad/baseline/__init__.py
+++
@@ -1,4 +0,0 @@
-from baseline.argument_parser import ArgumentParser
-from baseline.file_interface import FileInterface
-from baseline.processor import Processor, Sampler
-from baseline.model import Model, Loss
